7cec50276dd3fa3a1d1c8efd22c806458205a5ac
Leonardo da Silva <leonardo.leo2021@gmail.com>
2024-06-17 21:36:49 -0300

    Controle de Fluxo - Desafio

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+    public char[] getMessage(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMessage'");
+    }
+}
